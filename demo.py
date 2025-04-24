c54d42ba5160b4a5ef70625f3495fe39f21a1600
Kohei Takada <kohei.takada@groove-x.com>
2025-04-24 09:43:35 +0900

    Measure all processing units.

--- demo.py
+++ demo.py
@@ -61,17 +61,18 @@ norm = lambda x: (x - np.min(x)) / (np.max(x) - np.min(x) + 1e-9)
 
 def process_markers(pic, detector, exp, args, refine_corners, decode_markers):
     # Prepare image for YOLOX
-    preproc = ValTransform(legacy=False)
-    img, _ = preproc(pic, None, exp.test_size)
-    img = torch.from_numpy(img).unsqueeze(0)
-    img = img.float()
-    if args.device == "gpu":
-        img = img.cuda()
-        if args.fp16:
-            img = img.half()
+    with deeparuco_timer("preprocessing"):
+        preproc = ValTransform(legacy=False)
+        img, _ = preproc(pic, None, exp.test_size)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.float()
+        if args.device == "gpu":
+            img = img.cuda()
+            if args.fp16:
+                img = img.half()
 
     # Detect markers
-    with deeparuco_timer("detection"):
+    with deeparuco_timer("detection[torch]"):
         with torch.no_grad():
             outputs = detector(img)
             outputs = postprocess(
@@ -84,128 +85,132 @@ def process_markers(pic, detector, exp, args, refine_corners, decode_markers):
         print("No markers detected")
         return None
 
-    detections = outputs[0].cpu()
-    ratio = min(exp.test_size[0] / pic.shape[0], exp.test_size[1] / pic.shape[1])
-    bboxes = detections[:, 0:4]
-    bboxes /= ratio  # Scale back to original image size
+    with deeparuco_timer("postprocessing"):
+        detections = outputs[0].cpu()
+        ratio = min(exp.test_size[0] / pic.shape[0], exp.test_size[1] / pic.shape[1])
+        bboxes = detections[:, 0:4]
+        bboxes /= ratio  # Scale back to original image size
 
-    # Expanded bboxes
-    xyxy = [
-        [
-            int(max(det[0] - (0.2 * (det[2] - det[0]) + 0.5), 0)),
-            int(max(det[1] - (0.2 * (det[3] - det[1]) + 0.5), 0)),
-            int(min(det[2] + (0.2 * (det[2] - det[0]) + 0.5), pic.shape[1] - 1)),
-            int(min(det[3] + (0.2 * (det[3] - det[1]) + 0.5), pic.shape[0] - 1)),
+        # Expanded bboxes
+        xyxy = [
+            [
+                int(max(det[0] - (0.2 * (det[2] - det[0]) + 0.5), 0)),
+                int(max(det[1] - (0.2 * (det[3] - det[1]) + 0.5), 0)),
+                int(min(det[2] + (0.2 * (det[2] - det[0]) + 0.5), pic.shape[1] - 1)),
+                int(min(det[3] + (0.2 * (det[3] - det[1]) + 0.5), pic.shape[0] - 1)),
+            ]
+            for det in bboxes
         ]
-        for det in bboxes
-    ]
 
-    # Crop and normalize
-    crops_ori = [
-        cv2.resize(pic[det[1] : det[3], det[0] : det[2]], (64, 64)) for det in xyxy
-    ]
+        # Crop and normalize
+        crops_ori = [
+            cv2.resize(pic[det[1] : det[3], det[0] : det[2]], (64, 64)) for det in xyxy
+        ]
 
-    # Output crops
-    if args.get_crops:
-        for i in range(len(crops_ori)):
-            cv2.imwrite(f"crop_{i}.png", crops_ori[i])
+        # Output crops
+        if args.get_crops:
+            for i in range(len(crops_ori)):
+                cv2.imwrite(f"crop_{i}.png", crops_ori[i])
 
-    # Normalize (if not baseline!)
-    if args.regressor != "reg_baseline":
-        crops = [norm(crop) for crop in crops_ori]
-    else:
-        crops = crops_ori.copy()
+        # Normalize (if not baseline!)
+        if args.regressor != "reg_baseline":
+            crops = [norm(crop) for crop in crops_ori]
+        else:
+            crops = crops_ori.copy()
 
     # Refine corners
-    with deeparuco_timer("regression"):
+    with deeparuco_timer("regression[tf]"):
         corners = refine_corners(np.array(crops)).numpy()
 
-    # Convert to (x, y) pairs
-    if args.regressor.split("_")[1] == "hmap":
-        # Output hmaps
-        if args.get_heatmaps:
-            for i in range(corners.shape[0]):
-                cv2.imwrite(f"map_{i}.png", norm(corners[i]) * 255)
+    with deeparuco_timer("keypoint_detection"):
+        # Convert to (x, y) pairs
+        if args.regressor.split("_")[1] == "hmap":
+            # Output hmaps
+            if args.get_heatmaps:
+                for i in range(corners.shape[0]):
+                    cv2.imwrite(f"map_{i}.png", norm(corners[i]) * 255)
 
-        # Instantiate keypoint detector
-        area = 75  # <- Expected area of the blobs to detect
-        kp_params = cv2.SimpleBlobDetector_Params()
-        if area > 0:
-            kp_params.filterByArea = True
-            kp_params.minArea = area * 0.8
-            kp_params.maxArea = area * 1.2
-        kp_detector = cv2.SimpleBlobDetector_create(kp_params)
+            # Instantiate keypoint detector
+            area = 75  # <- Expected area of the blobs to detect
+            kp_params = cv2.SimpleBlobDetector_Params()
+            if area > 0:
+                kp_params.filterByArea = True
+                kp_params.minArea = area * 0.8
+                kp_params.maxArea = area * 1.2
+            kp_detector = cv2.SimpleBlobDetector_create(kp_params)
 
+            corners = [
+                [(x, y) for x, y in zip(*pos_from_heatmap(pred, kp_detector))]
+                for pred in corners
+            ]
+
+            # Discard detections if less than 4 corners
+            keep = [len(cs) == 4 for cs in corners]
+            xyxy, crops_ori, corners = zip(
+                *[
+                    (det, crop, cs)
+                    for det, crop, cs, k in zip(xyxy, crops_ori, corners, keep)
+                    if k == True
+                ]
+            )
+
+        else:
+            corners = [[(pred[i], pred[i + 1]) for i in range(0, 8, 2)] for pred in corners]
+
+        # Ensure corners are ordered
         corners = [
-            [(x, y) for x, y in zip(*pos_from_heatmap(pred, kp_detector))]
-            for pred in corners
+            ordered_corners([c[0] for c in cs], [c[1] for c in cs]) for cs in corners
         ]
 
-        # Discard detections if less than 4 corners
-        keep = [len(cs) == 4 for cs in corners]
-        xyxy, crops_ori, corners = zip(
-            *[
-                (det, crop, cs)
-                for det, crop, cs, k in zip(xyxy, crops_ori, corners, keep)
-                if k == True
-            ]
-        )
+        # Extract markers from corners (if 4 corners available)
+        markers = []
+        for crop, cs in zip(crops_ori, corners):
+            marker = marker_from_corners(crop, cs, 32)
+            # Grayscale and normalize
+            markers.append(norm(cv2.cvtColor(marker, cv2.COLOR_BGR2GRAY)))
 
-    else:
-        corners = [[(pred[i], pred[i + 1]) for i in range(0, 8, 2)] for pred in corners]
-
-    # Ensure corners are ordered
-    corners = [
-        ordered_corners([c[0] for c in cs], [c[1] for c in cs]) for cs in corners
-    ]
-
-    # Extract markers from corners (if 4 corners available)
-    markers = []
-    for crop, cs in zip(crops_ori, corners):
-        marker = marker_from_corners(crop, cs, 32)
-        # Grayscale and normalize
-        markers.append(norm(cv2.cvtColor(marker, cv2.COLOR_BGR2GRAY)))
-
-    if args.get_markers:
-        for i in range(len(markers)):
-            cv2.imwrite(f"marker_{i}.png", markers[i] * 255.0)
+    with deeparuco_timer("visualization1"):
+        if args.get_markers:
+            for i in range(len(markers)):
+                cv2.imwrite(f"marker_{i}.png", markers[i] * 255.0)
 
     # Get ids from markers
-    with deeparuco_timer("decode"):
+    with deeparuco_timer("decode[tf]"):
         decoder_out = np.round(decode_markers(np.array(markers)).numpy())
         ids, dists = zip(*[find_id(out) for out in decoder_out])
 
     # Visualize
-    line_width = 2  # Line width for drawing detections
-    for cs, det, id, dist in zip(corners, xyxy, ids, dists):
-        # Pack 2-by-2
-        cs = [(cs[i], cs[i + 1]) for i in range(0, 8, 2)]
+    with deeparuco_timer("visualization2"):
+        line_width = 2  # Line width for drawing detections
+        for cs, det, id, dist in zip(corners, xyxy, ids, dists):
+            # Pack 2-by-2
+            cs = [(cs[i], cs[i + 1]) for i in range(0, 8, 2)]
 
-        color = (0, 255, 0)
-        if dist >= args.threshold:
-            color = (0, 0, 255)
+            color = (0, 255, 0)
+            if dist >= args.threshold:
+                color = (0, 0, 255)
 
-        width = det[2] - det[0]
-        height = det[3] - det[1]
+            width = det[2] - det[0]
+            height = det[3] - det[1]
 
-        for i in range(0, 4):
-            p1 = (int(det[0] + cs[i][0] * width), int(det[1] + cs[i][1] * height))
-            p2 = (
-                int(det[0] + cs[(i + 1) % 4][0] * width),
-                int(det[1] + cs[(i + 1) % 4][1] * height),
+            for i in range(0, 4):
+                p1 = (int(det[0] + cs[i][0] * width), int(det[1] + cs[i][1] * height))
+                p2 = (
+                    int(det[0] + cs[(i + 1) % 4][0] * width),
+                    int(det[1] + cs[(i + 1) % 4][1] * height),
+                )
+                pic = cv2.line(pic, p1, p2, color, line_width, cv2.LINE_AA)
+
+            pic = cv2.putText(
+                pic,
+                str(id),
+                (det[0] + int(width / 2) - 20, det[1] + int(height / 2) + 5),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.75,
+                color,
+                line_width,
+                cv2.LINE_AA,
             )
-            pic = cv2.line(pic, p1, p2, color, line_width, cv2.LINE_AA)
-
-        pic = cv2.putText(
-            pic,
-            str(id),
-            (det[0] + int(width / 2) - 20, det[1] + int(height / 2) + 5),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.75,
-            color,
-            line_width,
-            cv2.LINE_AA,
-        )
 
     return pic
 
